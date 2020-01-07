7ca78c112715851d6d63adb20c52b953a78887cc
nggiest <gioanggiesta@gmail.com>
2020-01-07 21:33:47 +0700

    Revisi 7 Jan 2020 v3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,35 +28,77 @@ class HomeController extends Controller
     public function index()
     {
         $lab = Jadwallab::all();
-        $jadwallab1= DB::table('jadwallab')
+        $lab1= DB::table('jadwallab')
         ->join('makul','jadwallab.makul_id','=','makul.id')
         ->join('hari','jadwallab.hari_id','=','hari.id')
-        ->select('jadwalaslab.*','makul.nama_makul as nama')
+        ->select('jadwallab.*','makul.nama_makul as nama')
         ->where('jadwallab.jampel','1')
         ->orderBy('hari.id')
         ->get();
         $lab2= DB::table('jadwallab')
         ->join('makul','jadwallab.makul_id','=','makul.id')
         ->join('hari','jadwallab.hari_id','=','hari.id')
-        ->select('jadwalaslab.*','makul.nama_makul as nama')
+        ->select('jadwallab.*','makul.nama_makul as nama')
         ->where('jadwallab.jampel','2')
         ->orderBy('hari.id')
         ->get();
         $lab3= DB::table('jadwallab')
         ->join('makul','jadwallab.makul_id','=','makul.id')
         ->join('hari','jadwallab.hari_id','=','hari.id')
-        ->select('jadwalaslab.*','makul.nama_makul as nama')
+        ->select('jadwallab.*','makul.nama_makul as nama')
         ->where('jadwallab.jampel','3')
         ->orderBy('hari.id')
         ->get();
         $lab4= DB::table('jadwallab')
         ->join('makul','jadwallab.makul_id','=','makul.id')
         ->join('hari','jadwallab.hari_id','=','hari.id')
-        ->select('jadwalaslab.*','makul.nama_makul as nama')
+        ->select('jadwallab.*','makul.nama_makul as nama')
         ->where('jadwallab.jampel','4')
         ->orderBy('hari.id')
         ->get();
+        $lab5= DB::table('jadwallab')
+        ->join('makul','jadwallab.makul_id','=','makul.id')
+        ->join('hari','jadwallab.hari_id','=','hari.id')
+        ->select('jadwallab.*','makul.nama_makul as nama')
+        ->where('jadwallab.jampel','5')
+        ->orderBy('hari.id')
+        ->get();
+        $lab6= DB::table('jadwallab')
+        ->join('makul','jadwallab.makul_id','=','makul.id')
+        ->join('hari','jadwallab.hari_id','=','hari.id')
+        ->select('jadwallab.*','makul.nama_makul as nama')
+        ->where('jadwallab.jampel','6')
+        ->orderBy('hari.id')
+        ->get();
+        $lab7= DB::table('jadwallab')
+        ->join('makul','jadwallab.makul_id','=','makul.id')
+        ->join('hari','jadwallab.hari_id','=','hari.id')
+        ->select('jadwallab.*','makul.nama_makul as nama')
+        ->where('jadwallab.jampel','7')
+        ->orderBy('hari.id')
+        ->get();
+        $lab8= DB::table('jadwallab')
+        ->join('makul','jadwallab.makul_id','=','makul.id')
+        ->join('hari','jadwallab.hari_id','=','hari.id')
+        ->select('jadwallab.*','makul.nama_makul as nama')
+        ->where('jadwallab.jampel','8')
+        ->orderBy('hari.id')
+        ->get();
+        $lab9= DB::table('jadwallab')
+        ->join('makul','jadwallab.makul_id','=','makul.id')
+        ->join('hari','jadwallab.hari_id','=','hari.id')
+        ->select('jadwallab.*','makul.nama_makul as nama')
+        ->where('jadwallab.jampel','9')
+        ->orderBy('hari.id')
+        ->get();
+        $lab10= DB::table('jadwallab')
+        ->join('makul','jadwallab.makul_id','=','makul.id')
+        ->join('hari','jadwallab.hari_id','=','hari.id')
+        ->select('jadwallab.*','makul.nama_makul as nama')
+        ->where('jadwallab.jampel','10')
+        ->orderBy('hari.id')
+        ->get();
 
-        return view('home', compact('jadwallab1','lab2','lab3','lab4'));
+        return view('home', compact('lab1','lab2','lab3','lab4','lab5','lab6','lab7','lab8','lab9','lab10'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,24 +14,142 @@
                 <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
                     <thead>
                         <tr role="row">
-                        <th class="sorting" tabindex="0" aria-controls="example2" style="text-align:center" >Jam Pelajaran</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2" style="text-align:center" >Senin</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2" style="text-align:center" >Selasa</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2" style="text-align:center" >Rabu</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2" style="text-align:center" >Kamis</th>
-                        <th class="sorting" tabindex="0" aria-controls="example2"style="text-align:center" >Jumat</th>
-                        
+                        <th class="sorting" tabindex="0"  >Sesi</th>
+                        <th class="sorting" tabindex="0"  >Senin</th>
+                        <th class="sorting" tabindex="0" >Selasa</th>
+                        <th class="sorting" tabindex="0" >Rabu</th>
+                        <th class="sorting" tabindex="0" >Kamis</th>
+                        <th class="sorting" tabindex="0" >Jumat</th>
                         </tr>
                     </thead>
                     @php (
                         $no = 1
                     )
+                    <tbody>
                     <tr>
                     <td style="text-align:center">Jam 1</td>
-                    @foreach($jadwallab1 as $jab1 )
-                      <td style="text-align:center">@if(Auth::User())   <a href="{{route('lab.edit',$jab1->id)}}"> {{$jab1->nama}}
+                    @foreach($lab1 as $jab1 )
+                    @if(Auth::User())
+                      <td style="text-align:center">   <a href="{{route('lab.edit',$jab1->id)}}"> {{$jab1->nama}}
                       @else
+                      <td style="text-align:center"> <a href="{{route('lab.show',$jab1->id)}}"> {{$jab1->nama}}
                       {{$jab1->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+                    <tr>
+                    <td style="text-align:center">Jam 2</td>
+                    @foreach($lab2 as $jab2 )
+                    @if(Auth::User())
+                      <td style="text-align:center"><a href="{{route('lab.edit',$jab2->id)}}"> {{$jab2->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab2->id)}}">
+                      {{$jab2->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 3</td>
+                    @foreach($lab3 as $jab3 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab3->id)}}"> {{$jab3->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab3->id)}}">
+                      {{$jab3->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 4</td>
+                    @foreach($lab4 as $jab4 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab4->id)}}"> {{$jab4->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab4->id)}}">
+                      {{$jab4->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 5</td>
+                    @foreach($lab5 as $jab5 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab5->id)}}"> {{$jab5->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab5->id)}}">
+                      {{$jab5->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 6</td>
+                    @foreach($lab6 as $jab6 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab6->id)}}"> {{$jab6->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab6->id)}}">
+                      {{$jab6->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 7</td>
+                    @foreach($lab7 as $jab7 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab7->id)}}"> {{$jab7->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab7->id)}}">
+                      {{$jab7->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 8</td>
+                    @foreach($lab8 as $jab8 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab8->id)}}"> {{$jab7->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab8->id)}}">
+                      {{$jab8->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 9</td>
+                    @foreach($lab9 as $jab9 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab9->id)}}"> {{$jab9->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab9->id)}}">
+                      {{$jab9->nama}}
+                      @endif
+                      </td>
+                    @endforeach
+                    </tr>
+
+                    <tr>
+                    <td style="text-align:center">Jam 10</td>
+                    @foreach($lab10 as $jab10 )
+                    @if(Auth::User())
+                      <td style="text-align:center"> <a href="{{route('lab.edit',$jab10->id)}}"> {{$jab10->nama}}
+                      @else
+                      <td style="text-align:center"><a href="{{route('lab.show',$jab10->id)}}">
+                      {{$jab10->nama}}
                       @endif
                       </td>
                     @endforeach
